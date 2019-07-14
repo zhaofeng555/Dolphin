d5d54dded67dbbc244a385f43a898f61bf7036b7
haojg <443842006@qq.com>
2019-07-14 22:40:31 +0800

    sql

---
+++ db/db.sql
@@ -0,0 +1,13 @@
+use ds0;
+
+CREATE TABLE `t_demo_1` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `t_demo_0` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
